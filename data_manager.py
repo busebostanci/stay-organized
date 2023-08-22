6ecdf66de9574c7b99b1d44527bbdbb9faafe562
Buse Gonul Bostanci <busegonulbostanci@gmail.com>
2023-08-22 17:26:47 +0300

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,8 @@
 import json
 
-# Read the JSON data from the file
 with open('tasks.json', 'r') as json_file:
     tasks_data = json.load(json_file)
 
-# Now you can work with the tasks_data as a Python object
 for task in tasks_data:
     title = task['title']
     description = task['description']
